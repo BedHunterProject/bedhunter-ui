cb547797c17f4438eafbc94efd98d2e4415db758
LevaiViktoria <71004582+LevaiViktoria@users.noreply.github.com>
2022-04-22 01:19:35 +0200

    contact kártyák terv

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,6 +9,80 @@ const Contact = () => {
     <Banner title="Contact">
     </Banner>
   </HeroComponent>
+  <div>
+  <div class="container">
+  <div class="card">
+    <img src="https://lh3.googleusercontent.com/ytP9VP86DItizVX2YNA-xTYzV09IS7rh4WexVp7eilIcfHmm74B7odbcwD5DTXmL0PF42i2wnRKSFPBHlmSjCblWHDCD2oD1oaM1CGFcSd48VBKJfsCi4bS170PKxGwji8CPmehwPw=w200-h247-no" alt="Person" class="card__image"></img>
+    <p class="card__name">Contact1</p>
+    <div class="grid-container">
+
+      <div class="grid-child-posts">
+        Valami
+      </div>
+
+      <div class="grid-child-followers">
+        Valami
+      </div>
+
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a>icon</li>
+    </ul>
+    <button class="btn draw-border">Follow</button>
+    <button class="btn draw-border">Message</button>
+
+  </div>
+  <div class="card">
+    <img src="https://lh3.googleusercontent.com/oUUiPB9sq3ACq4bUaRmo8pgvC4FUpRRrQKcGIBSOsafawZfRpF1vruFeYt6uCfL6wGDQyvOi6Ez9Bpf1Fb7APKjIyVsft7FLGR6QqdRFTiceNQBm1In9aZyrXp33cZi9pUNqjHASdA=s170-no" alt="Person" class="card__image"></img>
+    <p class="card__name">Contact2</p>
+    <div class="grid-container">
+
+      <div class="grid-child-posts">
+        Valami
+      </div>
+
+      <div class="grid-child-followers">
+        Valami
+      </div>
+
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a>icon</li>
+    </ul>
+    <button class="btn draw-border">Follow</button>
+    <button class="btn draw-border">Message</button>
+  </div>
+  <div class="card">
+    <img src="https://lh3.googleusercontent.com/pZwZJ5HIL5iKbA91UGMUIPR0VJWa3K0vOGzDZmY6wU3EJBUdfsby3VEyxU162XxTyOyP3D154tjkr-4Jgcx8lygYUR8eB-jVmld4dsHi1c-mE_A8jKccseAG7bdEwVrcuuk6ciNtSw=s170-no" alt="Person" class="card__image"></img>
+    <p class="card__name">Contact3</p>
+    <div class="grid-container">
+
+      <div class="grid-child-posts">
+        Valami
+      </div>
+
+      <div class="grid-child-followers">
+        Valami
+      </div>
+
+    </div>
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-instagram"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a>icon</li>
+      <li><a href="#"><i class="fa fa-codepen"></i></a>icon</li>
+    </ul>
+    <button class="btn draw-border">Follow</button>
+    <button class="btn draw-border">Message</button>
+  </div>
+</div>
+</div>
 </>
 }
 export default Contact
